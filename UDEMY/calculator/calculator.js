425499b926d9bb1b6bb6949b5d6c7ae8f5f2642b
jagveergagaan <jagveergagaan@gmail.com>
2022-01-28 21:36:18 +0530

    BMI and simple calculator in EXPRESS JS

--- UDEMY/calculator/calculator.js
+++ UDEMY/calculator/calculator.js
@@ -1,9 +1,41 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/', (req, res) => {
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var calculate = num1 + num2;
+  res.send('The sum of number is :- ' + calculate);
+});
+
+app.get('/bmicalculator', (req, res) => {
+  res.sendFile(__dirname + '/bmicalculator.html');
+});
+
+app.post('/bmicalculator', (req, res) => {
+  var weight = parseFloat(req.body.weight);
+  var height = parseFloat(req.body.height);
+  var bmiResult = weight / (height * height);
+  res.send('Your BMI is :- ' + bmiResult);
+
+  // if (bmiResult < 19) {
+  //   return bmiRes;
+  // } else if (bmiResult > 19 && bmiResult < 25) {
+  //   return bmiRes;
+  // } else if (bmiResult > 25 && bmiResult < 30) {
+  //   return bmiRes;
+  // }else if(bmiResult > 30) (
+  //   return bmiRes;
+  // )
 });
 
 // setting port details here
